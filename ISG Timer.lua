993374f308126f07168c14fc9ba7b514f2495493
Isotarge <isaacjmiell@gmail.com>
2015-07-13 15:52:38 +0930

    ISG timer improvements
    - Works with all regions
    - Error messages for ROMs that are not supported
    - No global state

--- ISG Timer.lua
+++ ISG Timer.lua
@@ -2,18 +2,33 @@
 -- Written by Isotarge, 2015
 -- Based on research by Exchord
 
-prev_map = 0;
+local map;
+local romName = gameinfo.getromname();
 
-timer_value = 0;
-timer_start_frame = 0;
-timer_started = false;
+if bizstring.contains(romName, "Donkey Kong 64") then
+	if bizstring.contains(romName, "USA") then
+		map = 0x7444E7;
+	elseif bizstring.contains(romName, "Europe") then
+		map = 0x73EC37;
+	elseif bizstring.contains(romName, "Japan") then
+		map = 0x743DA7;
+	elseif bizstring.contains(romName, "Kiosk") then
+		console.log("The kiosk version is not supported.");
+		return;
+	end
+else
+	console.log("This game is not supported.");
+	return;
+end
 
--- TODO: Port to other regions/kiosk
-map = 0x7444E7;
+local prev_map = 0;
+local timer_value = 0;
+local timer_start_frame = 0;
+local timer_started = false;
 
-function timer ()
-	map_value = mainmemory.readbyte(map);
-	if (map_value == 153 and prev_map ~= 153) then
+local function timer ()
+	local map_value = mainmemory.readbyte(map);
+	if map_value == 153 and prev_map ~= 153 then
 		timer_value = 0;
 		timer_start_frame = emu.framecount();
 		timer_started = true;
@@ -31,8 +46,8 @@ function timer ()
 	end
 
 	if timer_started then
-		s = timer_value / 60;
-		timer_string = string.format("%.2d:%05.2f", s / 60 % 60, s % 60);
+		local s = timer_value / 60;
+		local timer_string = string.format("%.2d:%05.2f", s / 60 % 60, s % 60);
 		gui.text(16, 16, "ISG Timer: "..timer_string, null, null, 'topright');
 	else
 		gui.text(16, 16, "Waiting for ISG", null, null, 'topright');
