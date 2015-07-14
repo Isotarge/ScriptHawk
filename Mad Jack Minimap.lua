4fc04f06a910356bd44d5cb306c9a05d69e38478
Isotarge <isaacjmiell@gmail.com>
2015-07-14 14:17:52 +0930

    Mad Jack minimap work
    - Make tons of global stuff local
    - Only show minimap when actually in Mad Jack fight
    - Make sure it doesn't load if the game is not supported
    - Other misc cleanup

--- Mad Jack Minimap.lua
+++ Mad Jack Minimap.lua
@@ -1,59 +1,77 @@
 -- DK64 - Mad Jack Minimap
--- Written by Isotarge, 2014
+-- Written by Isotarge, 2014-2015
 
--- Make some shorthand versions of BizHawk's memory functions
-mrb = mainmemory.readbyte;
-mwb = mainmemory.write_u8;
-mru24 = mainmemory.read_u24_be;
-mrf = mainmemory.readfloat;
-mwf = mainmemory.writefloat;
+local map;
+local kong_model_pointer;
+local MJ_state_pointer;
 
-MJ_state_pointer = 0x7fdc91;
-script_root = "Lua/ScriptHawk";
+local romName = gameinfo.getromname();
+
+if bizstring.contains(romName, "Donkey Kong 64") then
+	if bizstring.contains(romName, "USA") then
+		map                = 0x7444E7;
+		kong_model_pointer = 0x7fbb4d;
+		MJ_state_pointer   = 0x7fdc91;
+	elseif bizstring.contains(romName, "Europe") then
+		map                = 0x73EC37;
+		kong_model_pointer = 0x7fba6d;
+		MJ_state_pointer   = 0x7fdc91; -- TODO: Find
+	elseif bizstring.contains(romName, "Japan") then
+		map                = 0x743DA7;
+		kong_model_pointer = 0x7fbfbd;
+		MJ_state_pointer   = 0x7fdc91; -- TODO: Find
+	elseif bizstring.contains(romName, "Kiosk") then
+		console.log("The kiosk version is not supported.");
+		return;
+	end
+else
+	console.log("This game is not supported.");
+	return;
+end
+
+local script_root = "Lua/ScriptHawk";
+local correct_map = 154;
 
 -- Mad Jack state
-MJ_time_until_next_action = 0x2d;
-MJ_actions_remaining      = 0x58;
-MJ_action_type            = 0x59;
-MJ_current_pos            = 0x60;
-MJ_next_pos               = 0x61;
-MJ_white_switch_pos       = 0x64;
-MJ_blue_switch_pos        = 0x65;
+local MJ_time_until_next_action = 0x2d;
+local MJ_actions_remaining      = 0x58;
+local MJ_action_type            = 0x59;
+local MJ_current_pos            = 0x60;
+local MJ_next_pos               = 0x61;
+local MJ_white_switch_pos       = 0x64;
+local MJ_blue_switch_pos        = 0x65;
 
 -- Colors
-MJ_blue         = 0x7f00a2e8;
-MJ_blue_switch  = 0xff00a2e8;
-MJ_white        = 0x7fffffff;
-MJ_white_switch = 0xffffffff;
+local MJ_blue         = 0x7f00a2e8;
+local MJ_blue_switch  = 0xff00a2e8;
+local MJ_white        = 0x7fffffff;
+local MJ_white_switch = 0xffffffff;
 
 -- Minimap ui
-MJ_minimap_x_offset  = 19;
-MJ_minimap_y_offset  = 19;
-MJ_minimap_width     = 16;
-MJ_minimap_height    = 16;
+local MJ_minimap_x_offset  = 19;
+local MJ_minimap_y_offset  = 19;
+local MJ_minimap_width     = 16;
+local MJ_minimap_height    = 16;
 
-MJ_minimap_text_x = MJ_minimap_x_offset + 4.5 * MJ_minimap_width;
-MJ_minimap_text_y = MJ_minimap_y_offset;
+local MJ_minimap_text_x = MJ_minimap_x_offset + 4.5 * MJ_minimap_width;
+local MJ_minimap_text_y = MJ_minimap_y_offset;
 
-MJ_minimap_phase_number_y      = MJ_minimap_text_y;
-MJ_minimap_actions_remaining_y = MJ_minimap_phase_number_y      + MJ_minimap_height;
-MJ_time_until_next_action_y    = MJ_minimap_actions_remaining_y + MJ_minimap_height;
+local MJ_minimap_phase_number_y      = MJ_minimap_text_y;
+local MJ_minimap_actions_remaining_y = MJ_minimap_phase_number_y      + MJ_minimap_height;
+local MJ_time_until_next_action_y    = MJ_minimap_actions_remaining_y + MJ_minimap_height;
 
-MJ_kong_row_y                  = MJ_time_until_next_action_y + MJ_minimap_height;
-MJ_kong_col_y                  = MJ_kong_row_y + MJ_minimap_height;
+local MJ_kong_row_y                  = MJ_time_until_next_action_y + MJ_minimap_height;
+local MJ_kong_col_y                  = MJ_kong_row_y + MJ_minimap_height;
 
-function round (x)
+local function round(x)
 	return x + 0.5 - (x + 0.5) % 1;
 end
 
--- Position stuff
-kong_model_pointer = 0x7fbb4d;
-
 -- Relative to kong model
-x_pos = 0x7c;
-z_pos = 0x84;
+local x_pos = 0x7c;
+local z_pos = 0x84;
 
-function position_to_rowcol (pos)
+local function position_to_rowcol(pos)
 	if pos < 450 then
 		return 0;
 	elseif pos < 570 then
@@ -72,17 +90,17 @@ function position_to_rowcol (pos)
 	return 7;
 end
 
-function get_kong_position ()
-	kong_model = mainmemory.read_u24_be(kong_model_pointer);
+local function get_kong_position()
+	local kong_model = mainmemory.read_u24_be(kong_model_pointer);
 
-	x = mainmemory.readfloat(kong_model + x_pos, true);
-	z = mainmemory.readfloat(kong_model + z_pos, true);
+	local x = mainmemory.readfloat(kong_model + x_pos, true);
+	local z = mainmemory.readfloat(kong_model + z_pos, true);
 
-	colseg = position_to_rowcol(z);
-	rowseg = position_to_rowcol(x);
+	local colseg = position_to_rowcol(z);
+	local rowseg = position_to_rowcol(x);
 
-	col = math.floor(colseg / 2);
-	row = math.floor(rowseg / 2);
+	local col = math.floor(colseg / 2);
+	local row = math.floor(rowseg / 2);
 
 	return {
 		["x"] = x, ["z"] = z,
@@ -91,19 +109,19 @@ function get_kong_position ()
 	};
 end
 
-function MJ_get_col_mask (position)
+local function MJ_get_col_mask(position)
 	return bit.band(position, 0x03);
 end
 
-function MJ_get_row_mask (position)
+local function MJ_get_row_mask(position)
 	return bit.rshift(bit.band(position, 0x0C), 2);
 end
 
-function MJ_get_switch_active_mask (position)
+local function MJ_get_switch_active_mask(position)
 	return bit.rshift(bit.band(position, 0x10), 4) > 0;
 end
 
-function MJ_get_color (col, row)
+local function MJ_get_color(col, row)
 	local color = 'blue';
 	if row % 2 == col % 2 then
 		color = 'white';
@@ -111,7 +129,7 @@ function MJ_get_color (col, row)
 	return color;
 end
 
-function MJ_get_action_type (phase_byte)
+local function MJ_get_action_type(phase_byte)
 	if phase_byte == 0x08 or phase_byte == 0x0a or phase_byte == 0x0b or phase_byte == 0x0c or phase_byte == 0x0e then
 		return "Jump";
 	elseif phase_byte == 0x01 or phase_byte == 0x05 then
@@ -122,7 +140,7 @@ function MJ_get_action_type (phase_byte)
 	return "Jump";
 end
 
-function MJ_get_phase (phase_byte)
+local function MJ_get_phase(phase_byte)
 	if phase_byte == 0x08 or phase_byte == 0x32 then
 		return 1;
 	elseif phase_byte == 0x0a or phase_byte == 0x2d then
@@ -137,7 +155,7 @@ function MJ_get_phase (phase_byte)
 	return 0;
 end
 
-function MJ_get_arrow_image (current, new)
+local function MJ_get_arrow_image(current, new)
 	if new.row > current.row then
 		if new.col > current.col then
 			return script_root.."/Images/up_right.png";
@@ -164,7 +182,7 @@ function MJ_get_arrow_image (current, new)
 	return script_root.."/Images/question-mark.png";
 end
 
-function MJ_parse_position (position)
+local function MJ_parse_position(position)
 	return {
 		["active"] = MJ_get_switch_active_mask(position),
 		["col"] = MJ_get_col_mask(position),
@@ -172,78 +190,80 @@ function MJ_parse_position (position)
 	};
 end
 
-function draw_mj_minimap ()
-	local MJ_state  = mru24(MJ_state_pointer);
+local function draw_mj_minimap()
+	-- Only draw minimap if the player is in the Mad Jack fight
+	if mainmemory.readbyte(map) == correct_map then
+		local MJ_state  = mainmemory.read_u24_be(MJ_state_pointer);
 
-	local cur_pos   = MJ_parse_position(mrb(MJ_state + MJ_current_pos));
-	local next_pos  = MJ_parse_position(mrb(MJ_state + MJ_next_pos));
+		local cur_pos   = MJ_parse_position(mainmemory.readbyte(MJ_state + MJ_current_pos));
+		local next_pos  = MJ_parse_position(mainmemory.readbyte(MJ_state + MJ_next_pos));
 
-	local white_pos = MJ_parse_position(mrb(MJ_state + MJ_white_switch_pos));
-	local blue_pos  = MJ_parse_position(mrb(MJ_state + MJ_blue_switch_pos));
+		local white_pos = MJ_parse_position(mainmemory.readbyte(MJ_state + MJ_white_switch_pos));
+		local blue_pos  = MJ_parse_position(mainmemory.readbyte(MJ_state + MJ_blue_switch_pos));
 
-	local switches_active = white_pos.active or blue_pos.active;
+		local switches_active = white_pos.active or blue_pos.active;
 
-	local row, col, x, y, color;
+		local row, col, x, y, color;
 
-	gui.clearGraphics();
+		gui.clearGraphics();
 
-	kong_position = get_kong_position();
+		local kong_position = get_kong_position();
 
-	for row=0,3 do
-		for	col=0,3 do
-			x = MJ_minimap_x_offset + col * MJ_minimap_width;
-			y = MJ_minimap_y_offset + (3 - row) * MJ_minimap_height;
+		for row=0,3 do
+			for	col=0,3 do
+				x = MJ_minimap_x_offset + col * MJ_minimap_width;
+				y = MJ_minimap_y_offset + (3 - row) * MJ_minimap_height;
 
-			color = MJ_blue;
-			if MJ_get_color(col, row) == 'white' then
-				color = MJ_white;
-			end
-
-			if switches_active then
-				if white_pos.row == row and white_pos.col == col and MJ_get_color(cur_pos.col, cur_pos.row) == 'white' then
-					color = MJ_white_switch;
-				elseif blue_pos.row == row and blue_pos.col == col and MJ_get_color(cur_pos.col, cur_pos.row) == 'blue' then
-					color = MJ_blue_switch;
+				color = MJ_blue;
+				if MJ_get_color(col, row) == 'white' then
+					color = MJ_white;
 				end
-			end
 
-			gui.drawRectangle(x, y, MJ_minimap_width, MJ_minimap_height, 0, color);
-
-			if switches_active then
-				if (white_pos.row == row and white_pos.col == col) or (blue_pos.row == row and blue_pos.col == col) then
-					gui.drawImage(script_root.."/Images/switch.png", x, y, MJ_minimap_width, MJ_minimap_height);
+				if switches_active then
+					if white_pos.row == row and white_pos.col == col and MJ_get_color(cur_pos.col, cur_pos.row) == 'white' then
+						color = MJ_white_switch;
+					elseif blue_pos.row == row and blue_pos.col == col and MJ_get_color(cur_pos.col, cur_pos.row) == 'blue' then
+						color = MJ_blue_switch;
+					end
 				end
-			end
 
-			if cur_pos.row == row and cur_pos.col == col then
-				gui.drawImage(script_root.."/Images/jack_icon.png", x, y, MJ_minimap_width, MJ_minimap_height);
-			elseif next_pos.row == row and next_pos.col == col then
-				gui.drawImage(MJ_get_arrow_image(cur_pos, next_pos), x, y, MJ_minimap_width, MJ_minimap_height);
-			end
+				gui.drawRectangle(x, y, MJ_minimap_width, MJ_minimap_height, 0, color);
 
-			if kong_position.row == row and kong_position.col == col then
-				--gui.drawText(x, y, "K");
-				gui.drawImage(script_root.."/Images/TinyFaceEdited.png", x, y, MJ_minimap_width, MJ_minimap_height);
+				if switches_active then
+					if (white_pos.row == row and white_pos.col == col) or (blue_pos.row == row and blue_pos.col == col) then
+						gui.drawImage(script_root.."/Images/switch.png", x, y, MJ_minimap_width, MJ_minimap_height);
+					end
+				end
+
+				if cur_pos.row == row and cur_pos.col == col then
+					gui.drawImage(script_root.."/Images/jack_icon.png", x, y, MJ_minimap_width, MJ_minimap_height);
+				elseif next_pos.row == row and next_pos.col == col then
+					gui.drawImage(MJ_get_arrow_image(cur_pos, next_pos), x, y, MJ_minimap_width, MJ_minimap_height);
+				end
+
+				if kong_position.row == row and kong_position.col == col then
+					gui.drawImage(script_root.."/Images/TinyFaceEdited.png", x, y, MJ_minimap_width, MJ_minimap_height);
+				end
 			end
 		end
-	end
 
-	-- Text info
-	local phase_byte = mrb(MJ_state + MJ_action_type);
-	local actions_remaining = mrb(MJ_state + MJ_actions_remaining);
-	local time_until_next_action = mrb(MJ_state + MJ_time_until_next_action);
+		-- Text info
+		local phase_byte = mainmemory.readbyte(MJ_state + MJ_action_type);
+		local actions_remaining = mainmemory.readbyte(MJ_state + MJ_actions_remaining);
+		local time_until_next_action = mainmemory.readbyte(MJ_state + MJ_time_until_next_action);
 
-	local phase = MJ_get_phase(phase_byte);
-	local action_type = MJ_get_action_type(phase_byte);
+		local phase = MJ_get_phase(phase_byte);
+		local action_type = MJ_get_action_type(phase_byte);
 
-	gui.drawText(MJ_minimap_text_x, MJ_minimap_actions_remaining_y, actions_remaining.." "..action_type.."s remaining");
+		gui.drawText(MJ_minimap_text_x, MJ_minimap_actions_remaining_y, actions_remaining.." "..action_type.."s remaining");
 
-	if action_type ~= "Jump" then
-		gui.drawText(MJ_minimap_text_x, MJ_minimap_phase_number_y  , "Phase "..phase.." (switch)");
-		gui.drawText(MJ_minimap_text_x, MJ_time_until_next_action_y, time_until_next_action.." ticks until next "..action_type);
-	else
-		gui.drawText(MJ_minimap_text_x, MJ_minimap_phase_number_y  , "Phase "..phase);
+		if action_type ~= "Jump" then
+			gui.drawText(MJ_minimap_text_x, MJ_minimap_phase_number_y  , "Phase "..phase.." (switch)");
+			gui.drawText(MJ_minimap_text_x, MJ_time_until_next_action_y, time_until_next_action.." ticks until next "..action_type);
+		else
+			gui.drawText(MJ_minimap_text_x, MJ_minimap_phase_number_y  , "Phase "..phase);
+		end
 	end
 end
 
-event.onframeend(draw_mj_minimap, "Mad Jack Minimap");
+event.onframestart(draw_mj_minimap, "Mad Jack Minimap");
