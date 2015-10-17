af60f75abb5b57cad740bc25db7d2ef3286f3587
Isotarge <isaacjmiell@gmail.com>
2015-10-18 08:19:18 +1030

    "See Levels" script for Impossible Mission

---
+++ Beta/See Levels.lua
@@ -0,0 +1,13 @@
+local map_base = 0x1b5a;
+local num_maps = 10 * 10;
+
+local function see_maps()
+	local i;
+	local value;
+	for i=0,num_maps do
+		value = mainmemory.readbyte(map_base + i);
+		mainmemory.writebyte(map_base + i, bit.bor(value, 0x80));
+	end
+end
+
+event.onframestart(see_maps, "See maps");
