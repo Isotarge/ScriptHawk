44be5d52ad7ee53f0f686c84609657220085041c
Isotarge <isaacjmiell@gmail.com>
2015-12-07 09:49:46 +1030

    Bot cleanup
    - Add some helper functions
    - Clean up some code

--- Beta/Tetris Attack Bot.lua
+++ Beta/Tetris Attack Bot.lua
@@ -9,14 +9,14 @@ local grid_height = 12;
 local grid_width = 6;
 
 colors = {
-	[0] = "Empty",
-	"Red",
-	"Green",
-	"Light Blue",
-	"Yellow",
-	"Purple",
-	"Dark Blue",
-	"!"
+	[0x00] = "Empty",
+	[0x01] = "Red",
+	[0x02] = "Green",
+	[0x03] = "Light Blue",
+	[0x04] = "Yellow",
+	[0x05] = "Purple",
+	[0x06] = "Dark Blue",
+	[0x07] = "!"
 };
 
 -- Status
@@ -27,6 +27,9 @@ colors = {
 -- 0x04 Red Block (can't move)
 -- 0x08 Grey Block (can't move)
 -- 0x40 Popping
+local unmoveableStates = {
+	0x04, 0x08, 0x40
+}
 
 -----------------
 -- UI Bollocks --
@@ -59,7 +62,7 @@ function drawUI()
 	-- Output current move to the screen
 	drawGridText(UI_HUD_LEFT_X_OFFSET,3,"Move");
 	if #moveQueue > 0 then
-		local currentMove = moveQueue[#moveQueue];
+		local currentMove = moveQueue[1];
 		drawGridText(UI_HUD_LEFT_X_OFFSET, 4, currentMove["x"]..","..currentMove["y"]);
 		drawGridText(UI_HUD_LEFT_X_OFFSET, 5, currentMove["type"]);
 	else
@@ -70,6 +73,14 @@ end
 --------------------
 -- The good stuff --
 --------------------
+
+function getCursorPosition()
+	local cursorLeftX = mainmemory.readbyte(cursor_left_x);
+	local cursorLeftY = mainmemory.readbyte(cursor_left_y) - 2;
+	--local cursorRightX = mainmemory.readbyte(cursor_right_x);
+	--local cursorRightY = mainmemory.readbyte(cursor_right_y) - 2;
+	return {["x"]=cursorLeftX, ["y"]=cursorLeftY};
+end
 
 function getGridAddress(x, y)
 	x = x - 2;
@@ -89,6 +100,17 @@ function getStatus(x, y)
 	return mainmemory.readbyte(getGridAddress(x, y) + 1);
 end
 
+function isMoveable(x, y)
+	local status = getStatus(x, y);
+	local i;
+	for i=1,#unmoveableStates do
+		if status == unmoveableStates[i] then
+			return false;
+		end
+	end
+	return true;
+end
+
 function getColumnHeight(x)
 	local y;
 	for y=1,grid_height do
@@ -99,30 +121,37 @@ function getColumnHeight(x)
 	return 0;
 end
 
+function rowContains(y, color)
+	local x;
+	for x=1,grid_width do
+		if getColor(x,y) == color then
+			return true;
+		end
+	end
+	return false;
+end
+
 local previousFrameA = false;
 function moveAt(x, y)
-	local cursorLeftX = mainmemory.readbyte(cursor_left_x);
-	local cursorLeftY = mainmemory.readbyte(cursor_left_y) - 2;
-	local cursorRightX = mainmemory.readbyte(cursor_right_x);
-	local cursorRightY = mainmemory.readbyte(cursor_right_y) - 2;
+	local cursorPosition = getCursorPosition();
 
-	if cursorLeftX < x then
+	if cursorPosition["x"] < x then
 		joypad.set({["Right"]=true},1);
 		previousFrameA = false;
-	elseif cursorLeftX > x then
+	elseif cursorPosition["x"] > x then
 		joypad.set({["Left"]=true},1);
 		previousFrameA = false;
 	end
 
-	if cursorLeftY < y then
+	if cursorPosition["y"] < y then
 		joypad.set({["Down"]=true},1);
 		previousFrameA = false;
-	elseif cursorLeftY > y then
+	elseif cursorPosition["y"] > y then
 		joypad.set({["Up"]=true},1);
 		previousFrameA = false;
 	end
 
-	if cursorLeftX == x and cursorLeftY == y then
+	if cursorPosition["x"] == x and cursorPosition["y"] == y then
 		previousFrameA = not previousFrameA;
 		joypad.set({["A"]=previousFrameA},1);
 		return true;
@@ -135,37 +164,30 @@ verbose = false;
 moveQueue = {};
 
 function getColorAtCursor()
-	local cursorLeftX = mainmemory.readbyte(cursor_left_x);
-	local cursorLeftY = mainmemory.readbyte(cursor_left_y) - 2;
-	local cursorRightX = mainmemory.readbyte(cursor_right_x);
-	local cursorRightY = mainmemory.readbyte(cursor_right_y) - 2;
-
-	local leftColor = getColor(cursorLeftX, cursorLeftY);
-	local rightColor = getColor(cursorRightX, cursorRightY);
-	return {colors[leftColor], colors[rightColor]};
+	local cursorPosition = getCursorPosition();
+	local leftColor = getColor(cursorPosition["x"], cursorPosition["y"]);
+	local rightColor = getColor(cursorPosition["x"] + 1, cursorPosition["y"]);
+	return {leftColor, rightColor};
 end
 
 function checkVertical3(x,y)
 	if verbose then
-		print("checking vertical 3 at "..x..","..y);
+		print("Checking vertical 3 at "..x..","..y);
 	end
 
-	local tls = getStatus(x,     y);
-	local trs = getStatus(x + 1, y);
-	local mls = getStatus(x,     y + 1);
-	local mrs = getStatus(x + 1, y + 1);
-	local bls = getStatus(x    , y + 2);
-	local brs = getStatus(x + 1, y + 2);
+	local tlm = isMoveable(x,     y);
+	local trm = isMoveable(x + 1, y);
+	local mlm = isMoveable(x,     y + 1);
+	local mrm = isMoveable(x + 1, y + 1);
+	local blm = isMoveable(x    , y + 2);
+	local brm = isMoveable(x + 1, y + 2);
 
-	local redBlock = 0x04;
-	local greyBlock = 0x08;
-	local popping = 0x40;
-	local statusArray = {tls, trs, mls, mrs, bls, brs};
+	local moveableArray = {tlm, trm, mlm, mrm, blm, brm};
 	local i;
-	for i=1,#statusArray do
-		if statusArray[i] == popping or statusArray[i] == redBlock or statusArray[i] == greyBlock then
+	for i=1,#moveableArray do
+		if moveableArray[i] == false then
 			if verbose then
-				print("a block was unmovable, skipping check at "..x..","..y);
+				print("A block was unmovable, skipping check at "..x..","..y);
 			end
 			return;
 		end
@@ -177,13 +199,13 @@ function checkVertical3(x,y)
 	local mr = getColor(x + 1, y + 1);
 	local bl = getColor(x    , y + 2);
 	local br = getColor(x + 1, y + 2);
-	
+
 	if verbose then
 		local colorArray = {tl, tr, ml, mr, bl, br};
 		local colorArrayFriendly = {colors[tl], colors[tr], colors[ml], colors[mr], colors[bl], colors[br]};
 		print(colorArrayFriendly);
 	end
-	
+
 	-- Check top row
 	if (tl == mr and mr == br) or (tr == ml and ml == bl) then
 		if tl ~= 0 and tr ~= 0 and tl ~= tr then
@@ -228,23 +250,25 @@ end
 function mainLoop()
 	drawUI();
 	if #moveQueue > 0 then
-		local currentMove = moveQueue[#moveQueue];
+		local currentMove = moveQueue[1];
 		local cL = getColor(currentMove["x"], currentMove["y"]);
 		local cR = getColor(currentMove["x"] + 1, currentMove["y"]);
 		if cL ~= 0 or cR ~= 0 then
-			local result = moveAt(currentMove["x"], currentMove["y"]);
-			if result == true then
+			if moveAt(currentMove["x"], currentMove["y"]) then
+				if verbose then
+					print("Move completed!");
+				end
 				findMoveGreedy();
 			end
 		else
 			if verbose then
-				print("both squares were empty, finding new move");
+				print("Both squares were empty, finding new move");
 			end
 			findMoveGreedy();
 		end
 	else
 		if verbose then
-			print("no moves in queue, finding new move");
+			print("No moves in queue, finding new move");
 		end
 		findMoveGreedy();
 	end
