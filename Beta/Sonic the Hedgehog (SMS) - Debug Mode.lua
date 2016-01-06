b8b8f35eff080cfdbdbf87a2e5e81cedcc1dba50
Isotarge <isaacjmiell@gmail.com>
2016-01-06 22:14:20 +1030

    Simple Sonic the Hedgehog (SMS) debug mode
    Thanks to The8BitBeast for writing this

---
+++ Beta/Sonic the Hedgehog (SMS) - Debug Mode.lua
@@ -0,0 +1,109 @@
+-------------------------------------------------------
+-- A simple debug mode for Sonic the Hedgehog on SMS --
+-- Written by The8BitBeast, 2016                     --
+-------------------------------------------------------
+
+local speed = 0x02;
+local max_speed = 0x20;
+local buttons = 0;
+
+local function getButton()
+	local bT = joypad.get();
+
+	local up = bT["P1 Up"];
+	local down = bT["P1 Down"];
+	local left = bT["P1 Left"];
+	local right = bT["P1 Right"];
+
+	if not up and not down and not left and not right then
+		return 0;
+	end
+
+	if up and not down and not left and not right then
+		return 1;
+	end
+
+	if up and not down and not left and right then
+		return 2;
+	end
+
+	if not up and not down and not left and right then
+		return 3;
+	end
+
+	if not up and down and not left and right then
+		return 4;
+	end
+
+	if not up and down and not left and not right then
+		return 5;
+	end
+
+	if not up and down and left and not right then
+		return 6;
+	end
+
+	if not up and not down and left and not right then
+		return 7;
+	end
+
+	if up and not down and left and not right then
+		return 8;
+	end
+end
+
+local function debug()
+	if forms.ischecked(debugCheck) then
+		local buttons = getButton();
+
+		mainmemory.write_s8(0x1403,0x00);
+		mainmemory.write_s8(0x1404,0x00);
+		mainmemory.write_u8(0x1405,0x00);
+		mainmemory.write_s8(0x1406,0x00);
+		mainmemory.write_s8(0x1407,0x00);
+		mainmemory.write_u8(0x1408,0x00);
+
+		if buttons == 1 then -- Up
+			mainmemory.write_s8(0x1407,0x00 - speed);
+			mainmemory.write_u8(0x1408,0xFF);
+		elseif buttons == 2 then -- Up + Right
+			mainmemory.write_s8(0x1407,0x00 - speed);
+			mainmemory.write_u8(0x1408,0xFF);
+			mainmemory.write_s8(0x1404,0x00 + speed);
+		elseif buttons == 3 then -- Right
+			mainmemory.write_s8(0x1404,0x00 + speed);
+		elseif buttons == 4 then -- Down + Right
+			mainmemory.write_s8(0x1404,0x00 + speed);
+			mainmemory.write_s8(0x1407,0x00 + speed);
+		elseif buttons == 5 then -- Down
+			mainmemory.write_s8(0x1407,0x00 + speed);
+		elseif buttons == 6 then -- Down + Left
+			mainmemory.write_s8(0x1404,0x00 - speed);
+			mainmemory.write_u8(0x1405,0xFF);
+			mainmemory.write_s8(0x1407,0x00 + speed);
+		elseif buttons == 7 then -- Left
+			mainmemory.write_s8(0x1404,0x00 - speed);
+			mainmemory.write_u8(0x1405,0xFF);
+		elseif buttons == 8 then -- Up + Left
+			mainmemory.write_s8(0x1404,0x00 - speed);
+			mainmemory.write_u8(0x1405,0xFF);
+			mainmemory.write_s8(0x1407,0x00 - speed);
+			mainmemory.write_u8(0x1408,0xFF);
+		end
+	end
+end
+
+local function increment_speed()
+	speed = math.min(max_speed, speed + 1);
+end
+
+local function decrement_speed()
+	speed = math.max(1, speed - 1);
+end
+
+formhandle = forms.newform(500, 250, "Debug");
+debugCheck = forms.checkbox(formhandle, "Debug On", 50, 50);
+button_increment_speed = forms.button(formhandle, "+", increment_speed, 50, 70, 20, 20);
+button_decrement_speed = forms.button(formhandle, "-", decrement_speed, 150, 70, 20, 20);
+
+event.onframestart(debug, "Debug Mode");
