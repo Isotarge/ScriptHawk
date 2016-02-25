b5fc173381f72fa28fd93e3fcdadecc89ae1c2a1
Isotarge <isaacjmiell@gmail.com>
2016-02-26 03:46:46 +1030

    Golf

--- Beta/remove_klump.lua
+++ Beta/remove_klump.lua
@@ -1,11 +1,11 @@
-d=0x80000000;r=mainmemory.read_u32_be;w=mainmemory.writebyte
+d=0x80000000;m=mainmemory;r=m.read_u32_be;w=m.writebyte
 function f()
 	for i=0,255 do
 		o=r(0x7FBFF0+i*4)
 		if o>d and o<d+d/10 then
 			o=o-d
-			if r(o+0x58)==187 then
-				w(o+0x63,0);w(o+0x154,0x37)
+			if r(o+88)==187 then
+				w(o+99,0);w(o+340,55)
 			end
 		end
 	end
