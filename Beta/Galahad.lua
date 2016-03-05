34e36d9eea2ca15b8362a1bbedd11e3b384f4207
Isotarge <isaacjmiell@gmail.com>
2016-03-05 12:52:23 +1030

    Galahad Stuff

--- Beta/Galahad.lua
+++ Beta/Galahad.lua
@@ -3,31 +3,63 @@
 -- Written by The8BitBeast, 2016                         --
 -----------------------------------------------------------
 
-local obj_index = 1;
-local obj_size = 0x44;
-local obj_start = 0x22B8;
+--------------
+-- UI State --
+--------------
+
+local object_index = 1;
 local max_objects = 51;
 
---Form Controls
-local formhandle;
-local obj_extra_data;
-local button_incr_obj_index;
-local button_decr_obj_index;
+function toHexString(value, desiredLength, prefix)
+	value = string.format("%X", value or 0);
+	prefix = prefix or "0x";
+	desiredLength = desiredLength or string.len(value);
+	while string.len(value) < desiredLength do
+		value = "0"..value;
+	end
+	return prefix..value;
+end
 
-local obj_x = 0x00; -- u16_be
-local obj_y = 0x02; -- u16_be
-local obj_cycle1 = 0x0C; -- u16 be
-local obj_cycle2 = 0x10; -- u16_be
-local obj_cycle3 = 0x14; -- u16_be
-local obj_cycle4 = 0x20; -- u16_be Check these cycles
-local obj_cycle4_bound = 0x24; -- u16_be
-local obj_type = 0x28; -- u16_be
-local obj_destructability = 0x2A; --Check this
-local obj_health = 0x2C; -- u16_be
-local obj_chest_type = 0x3A; -- u16_be
-local obj_held_item = 0x3C; -- u16_be
+local function increment_object_index()
+	object_index = math.min(max_objects, object_index + 1);
+end
 
---Tables
+local function decrement_object_index()
+	object_index = math.max(1, object_index - 1);
+end
+
+local formhandle = forms.newform(320, 200, "Galahad");
+local checkbox_extra_data = forms.checkbox(formhandle, "Show all", 16, 16);
+local button_increment_object_index = forms.button(formhandle, "-", decrement_object_index, 16, 48, 20, 20);
+local button_decrement_object_index = forms.button(formhandle, "+", increment_object_index, 48, 48, 20, 20);
+
+------------------------------
+-- Object Model Information --
+------------------------------
+
+local object_size = 0x44;
+local object_start = 0x22B8;
+
+local object_x = 0x00; -- u16_be
+local object_y = 0x02; -- u16_be
+local object_cycle1 = 0x0C; -- u16 be
+local object_cycle2 = 0x10; -- u16_be
+local object_cycle3 = 0x14; -- u16_be
+
+local object_respawn_timer = 0x1E; -- s16_be
+local object_respawn_timer_cap = 0x22; -- s16_be
+local object_respawn_x_position = 0x40;
+local object_respawn_y_position = 0x42;
+
+local object_cycle4 = 0x20; -- u16_be Check these cycles
+local object_cycle4_bound = 0x24; -- u16_be
+local object_animation_frame = 0x26; -- u16_be
+local object_type = 0x28; -- u16_be
+local object_destructibility = 0x2A; --Check this
+local object_health = 0x2C; -- u16_be
+local object_chest_type = 0x3A; -- u16_be
+local object_held_item = 0x3C; -- u16_be
+
 local object_types = {
 	[0x00] = "Null",
 	[0x01] = "Shop",
@@ -38,104 +70,101 @@ local object_types = {
 
 	[0x07] = "Swinging Ball on Chain",
 
+	[0x08] = "Destroyed Object / Pickup Spawner",
 	[0x09] = "Fancy Spike",
 	[0x0A] = "Guard",
 
 	[0x0C] = "Waterfall Log",
 	[0x0D] = "Big Plant Enemy",
-	[0x0E] = "Health Pickup",
-	[0x0F] = "Destroyed object / Pickup Spawner",
+	[0x0E] = "Pickup", -- Health, Coin
+	[0x0F] = "Destroyed Object / Pickup Spawner",
 	[0x10] = "Destroyed Object / Pickup Spawner",
 	[0x11] = "Roof Chain Spike Thing",
 	[0x12] = "Grey Floor Spike",
 	[0x13] = "Ant Enemy",
 	[0x14] = "Killable Glitch Dragon Thing (W2 tileset)",
 
-	--[0x16] = "",
-	--[0x17] = "",
-	--[0x18] = "",
-	--[0x19] = "",
-	--[0x1A] = "",
-	--[0x1B] = "",
-	--[0x1C] = "",
-	--[0x1D] = "",
-	--[0x1E] = "",
-	--[0x1F] = "",
-	--[0x20] = "",
-	--[0x21] = "",
-	--[0x22] = "",
-	--[0x23] = "",
-    --[0x24] = "",
+	[0x20] = "Chest",
+	[0x21] = "Level End Portal",
+
+	[0x38] = "Spawning Object", -- Guard?
 	--TODO: Finish Table
 };
 
 local held_item = {
 	[0x00] = "Nothing",
 	[0x01] = "2 coins",
+
 	--TODO: Finish Table
 };
 
--- Code
-function getExamineData(pointer)
-	local examine_data = {};
-	table.insert(examine_data,{"Object Number", obj_index});
+----------
+-- Code --
+----------
 
-	local objTypeValue = mainmemory.read_u16_be(pointer + obj_type);
-	if type(object_types[objTypeValue]) ~= "nil" then
-		objTypeValue = bizstring.hex(objTypeValue)..": "..object_types[objTypeValue];
-	else
-		objTypeValue = bizstring.hex(objTypeValue)..": Unknown";
-	end
-	table.insert(examine_data, {"Obj Type", objTypeValue});
-
-	table.insert(examine_data, {"X", mainmemory.read_u16_be(pointer + obj_x)});
-	table.insert(examine_data, {"Y", mainmemory.read_u16_be(pointer + obj_y)});
-	table.insert(examine_data, {"Health", mainmemory.read_u16_be(pointer + obj_health)});
-
-	local objHeldValue = mainmemory.read_u16_be(pointer + obj_held_item);
-	if type(held_item[objHeldValue]) ~= "nil" then
-		objHeldValue = bizstring.hex(objHeldValue)..": "..held_item[objHeldValue];
-	else
-		objHeldValue = bizstring.hex(objHeldValue)..": Respawn Timer / No item";
-	end
-	table.insert(examine_data, {"Held item", objHeldValue});
-
-	if forms.ischecked(obj_extra_data) then
-		table.insert(examine_data, {"0x04", mainmemory.read_u16_be(pointer + 0x04)});
-		table.insert(examine_data, {"0x06", mainmemory.read_u16_be(pointer + 0x06)});
-		table.insert(examine_data, {"0x08", mainmemory.read_u16_be(pointer + 0x08)});
-		table.insert(examine_data, {"0x0A", mainmemory.read_u16_be(pointer + 0x0A)});
-		table.insert(examine_data, {"0x0C: Cycle 1", mainmemory.read_u16_be(pointer + 0x0C)});
-		table.insert(examine_data, {"0x0E", mainmemory.read_u16_be(pointer + 0x0E)});
-		table.insert(examine_data, {"0x10: Cycle 2", mainmemory.read_u16_be(pointer + 0x10)});
-		table.insert(examine_data, {"0x12", mainmemory.read_u16_be(pointer + 0x12)});
-		table.insert(examine_data, {"0x14: Cycle 3", mainmemory.read_u16_be(pointer + 0x14)});
-		table.insert(examine_data, {"0x16", mainmemory.read_u16_be(pointer + 0x16)});
-		table.insert(examine_data, {"0x18", mainmemory.read_u16_be(pointer + 0x18)});
-		table.insert(examine_data, {"0x1A", mainmemory.read_u16_be(pointer + 0x1A)});
-		table.insert(examine_data, {"0x1C", mainmemory.read_u16_be(pointer + 0x1C)});
-		table.insert(examine_data, {"0x1E", mainmemory.read_u16_be(pointer + 0x1E)});
-		table.insert(examine_data, {"0x20: Cycle 4", mainmemory.read_u16_be(pointer + 0x20)});
-		table.insert(examine_data, {"0x22", mainmemory.read_u16_be(pointer + 0x22)});
-		table.insert(examine_data, {"0x24: Bound for cycle 4", mainmemory.read_u16_be(pointer + 0x24)});
-		table.insert(examine_data, {"0x26", mainmemory.read_u16_be(pointer + 0x26)});
-		table.insert(examine_data, {"0x2A: Destructibility", mainmemory.read_u16_be(pointer + 0x2A)});
-		table.insert(examine_data, {"0x2E", mainmemory.read_u16_be(pointer + 0x2E)});
-		table.insert(examine_data, {"0x30", mainmemory.read_u16_be(pointer + 0x30)});
-		table.insert(examine_data, {"0x32", mainmemory.read_u16_be(pointer + 0x32)});
-		table.insert(examine_data, {"0x34", mainmemory.read_u16_be(pointer + 0x34)});
-		table.insert(examine_data, {"0x36", mainmemory.read_u16_be(pointer + 0x36)});
-		table.insert(examine_data, {"0x38", mainmemory.read_u16_be(pointer + 0x38)});
-		table.insert(examine_data, {"0x3A: Chest Type", mainmemory.read_u16_be(pointer + obj_chest_type)});
-		table.insert(examine_data, {"0x3E", mainmemory.read_u16_be(pointer + 0x3E)});
-		table.insert(examine_data, {"0x40", mainmemory.read_u16_be(pointer + 0x40)});
-		table.insert(examine_data, {"0x42", mainmemory.read_u16_be(pointer + 0x42)});
-	end
-	return examine_data;
+function getObjectBase(index)
+	return object_start + object_size * (index - 1);
 end
 
-function fetch_address(index)
-	obj_address = obj_start + obj_size * (index - 1)
+function getExamineData(objectBase)
+	local examine_data = {};
+	table.insert(examine_data, {"Object Index", object_index.." ("..toHexString(objectBase)..")"});
+
+	local objectTypeValue = mainmemory.read_u16_be(objectBase + object_type);
+	if type(object_types[objectTypeValue]) ~= "nil" then
+		objectTypeValue = object_types[objectTypeValue].." ("..toHexString(objectTypeValue)..")";
+	else
+		objectTypeValue = "Unknown ("..toHexString(objectTypeValue)..")";
+	end
+	table.insert(examine_data, {"Object Type", objectTypeValue});
+
+	table.insert(examine_data, {"X", mainmemory.read_u16_be(objectBase + object_x)});
+	table.insert(examine_data, {"Y", mainmemory.read_u16_be(objectBase + object_y)});
+	table.insert(examine_data, {"Animation Frame", mainmemory.read_u16_be(objectBase + object_animation_frame)});
+
+	table.insert(examine_data, {"Health", mainmemory.read_u16_be(objectBase + object_health)});
+	table.insert(examine_data, {"Destructibility", mainmemory.read_u16_be(objectBase + object_destructibility)});
+
+	local objHeldValue = mainmemory.read_u16_be(objectBase + object_held_item);
+	if type(held_item[objHeldValue]) ~= "nil" then
+		objHeldValue = held_item[objHeldValue].." ("..toHexString(objHeldValue)..")";
+	else
+		objHeldValue = "Respawn Timer / No item ("..toHexString(objHeldValue)..")";
+	end
+	table.insert(examine_data, {"Held item", objHeldValue});
+	table.insert(examine_data, {"Chest Type", mainmemory.read_u16_be(objectBase + object_chest_type)});
+
+	table.insert(examine_data, {"Cycle 1", mainmemory.read_u16_be(objectBase + object_cycle1)});
+	table.insert(examine_data, {"Cycle 2", mainmemory.read_u16_be(objectBase + object_cycle2)});
+	table.insert(examine_data, {"Cycle 3", mainmemory.read_u16_be(objectBase + object_cycle3)});
+	table.insert(examine_data, {"Cycle 4", mainmemory.read_u16_be(objectBase + object_cycle4)});
+	table.insert(examine_data, {"Cycle 4 Bound", mainmemory.read_u16_be(objectBase + object_cycle4_bound)});
+
+	table.insert(examine_data, {"Respawn Timer", mainmemory.read_s16_be(objectBase + object_respawn_timer)});
+	table.insert(examine_data, {"Respawn Timer Cap", mainmemory.read_s16_be(objectBase + object_respawn_timer_cap)});
+	table.insert(examine_data, {"Respawn X Position", mainmemory.read_u16_be(objectBase + object_respawn_x_position)});
+	table.insert(examine_data, {"Respawn Y Position", mainmemory.read_u16_be(objectBase + object_respawn_y_position)});
+
+	if forms.ischecked(checkbox_extra_data) then
+		table.insert(examine_data, {"0x04", mainmemory.read_u16_be(objectBase + 0x04)});
+		table.insert(examine_data, {"0x06", mainmemory.read_u16_be(objectBase + 0x06)});
+		table.insert(examine_data, {"0x08", mainmemory.read_u16_be(objectBase + 0x08)});
+		table.insert(examine_data, {"0x0A", mainmemory.read_u16_be(objectBase + 0x0A)});
+		table.insert(examine_data, {"0x0E", mainmemory.read_u16_be(objectBase + 0x0E)});
+		table.insert(examine_data, {"0x12", mainmemory.read_u16_be(objectBase + 0x12)}); -- Cycle
+		table.insert(examine_data, {"0x16", mainmemory.read_u16_be(objectBase + 0x16)}); -- Cycle cap
+		table.insert(examine_data, {"0x18", mainmemory.read_u16_be(objectBase + 0x18)});
+		table.insert(examine_data, {"0x1A", mainmemory.read_u16_be(objectBase + 0x1A)});
+		table.insert(examine_data, {"0x1C", mainmemory.read_u16_be(objectBase + 0x1C)});
+		table.insert(examine_data, {"0x2E", mainmemory.read_u16_be(objectBase + 0x2E)});
+		table.insert(examine_data, {"0x30", mainmemory.read_u16_be(objectBase + 0x30)});
+		table.insert(examine_data, {"0x32", mainmemory.read_u16_be(objectBase + 0x32)});
+		table.insert(examine_data, {"0x34", mainmemory.read_u16_be(objectBase + 0x34)});
+		table.insert(examine_data, {"0x36", mainmemory.read_u16_be(objectBase + 0x36)});
+		table.insert(examine_data, {"0x38", mainmemory.read_u16_be(objectBase + 0x38)});
+		table.insert(examine_data, {"0x3E", mainmemory.read_u16_be(objectBase + 0x3E)});
+	end
+	return examine_data;
 end
 
 function draw_ui()
@@ -144,7 +173,7 @@ function draw_ui()
 	local row = 0;
 	local height = 16;
 
-	local examine_data = getExamineData(fetch_address(obj_index));
+	local examine_data = getExamineData(getObjectBase(object_index));
 	for i = #examine_data, 1, -1 do
 		if examine_data[i][1] ~= "Separator" then
 			gui.text(gui_x, gui_y + height * row, examine_data[i][1]..": "..examine_data[i][2], nil, nil, 'bottomright');
@@ -154,18 +183,5 @@ function draw_ui()
 		end
 	end
 end
-
-local function incr_obj_index()
-	obj_index = math.min(max_objects, obj_index + 1);
-end
-
-local function decr_obj_index()
-	obj_index = math.max(1, obj_index - 1);
-end
-
-formhandle = forms.newform(500, 250, "Galahad");
-obj_extra_data = forms.checkbox(formhandle, "Show all", 50, 50);
-button_incr_obj_index = forms.button(formhandle, "+", incr_obj_index, 50, 70, 20, 20);
-button_decr_obj_index = forms.button(formhandle, "-", decr_obj_index, 150, 70, 20, 20);
 
 event.onframestart(draw_ui , "ScriptHawk - Galahad object list OSD");
