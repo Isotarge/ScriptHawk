da0485cffcf6f07c831da11bd85e02db3fc0bf98
Isotarge <isaacjmiell@gmail.com>
2016-03-10 20:15:26 +1030

    Minor TFTT script fix

--- Beta/TFTT Log.lua
+++ Beta/TFTT Log.lua
@@ -2,7 +2,6 @@ local men = 0xE264; -- u16be
 local breed_ticker = 0xE262; -- fixed 12.4 be
 logging = true;
 data = {};
-local fixed_scaling_factor = 0.0125;
 
 local prev_breed_ticker = 0;
 
@@ -10,7 +9,7 @@ function log()
 	if not emu.islagged() then
 		if logging then
 			local _men = mainmemory.read_u16_be(men);
-			local _breed_ticker = mainmemory.read_u16_be(breed_ticker) * fixed_scaling_factor;
+			local _breed_ticker = mainmemory.read_u16_be(breed_ticker) / 16;
 			if _breed_ticker ~= prev_breed_ticker then
 				table.insert(data, {emu.framecount(), _men, _breed_ticker});
 				prev_breed_ticker = _breed_ticker;
@@ -22,7 +21,11 @@ function log()
 		if not logging then
 			print("Frames,Men,Counter");
 			for k, v in pairs(data) do
-				print(k..","..v[2]..","..v[3]..",");
+				local builtString = "";
+				for l, w in pairs(v) do
+					builtString = builtString..w..",";
+				end
+				print(builtString);
 			end
 			data = {};
 		end
