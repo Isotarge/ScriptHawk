894b44197cc190039971348f6336f889be1ca97d
Isotarge <isaacjmiell@gmail.com>
2016-03-20 01:59:55 +1030

    BK: Actor Spawner Improvements
    - Set a couple of object flags as per runehero's C source code release
    for the actor spawner
    - Scribble down last spawned object pointer in global memory for
    research purposes

--- Docs/BK ASM Hacking/Actor Spawner.asm
+++ Docs/BK ASM Hacking/Actor Spawner.asm
@@ -11,19 +11,42 @@ PUSH a0
 PUSH a1
 PUSH a2
 PUSH a3
+PUSH v0
 
 LH a0 SpawnActivationFlag
 BEQI a0 0 SkipSpawn
-LH a0 SpawnActorID
-LA a1 SpawnLocationArray
-JAL @SpawnActor //SpawnActor(u16 id, float[] position)
 NOP
 
-// Kill the spawn flag
-LI a0 0
-SH a0 SpawnActivationFlag
+	LH a0 SpawnActorID
+	LA a1 SpawnLocationArray
+	JAL @SpawnActor //SpawnActor(u16 id, float[] position)
+	NOP
+
+	// Skip setting the flags if the spawn didn't succeed
+	BEQI v0 0 SkipFlags
+	NOP
+
+	// Set the "spawn_item_id" to 0x03 as per the C source released by runehero
+	LI a0 3
+	SW a0 0xBC(v0)
+
+	// Set the "flag2" to 0x01 as per the C source released by runehero
+	LI a0 1
+	SB a0 0xEB(v0)
+
+	SkipFlags:
+
+	// Output the pointer to global memory so ScriptHawk can see it
+	LA a0 SpawnedActorPointer
+	SW v0 0(a0)
+
+	// Kill the spawn flag
+	LI a0 0
+	SH a0 SpawnActivationFlag
 
 SkipSpawn:
+
+POP v0
 POP a3
 POP a2
 POP a1
@@ -41,4 +64,6 @@ SpawnActorID:
 SpawnLocationArray:
 .word 0
 .word 0
+.word 0
+SpawnedActorPointer:
 .word 0
