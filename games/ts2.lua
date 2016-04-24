5bfa0ff14e5c2c4fd730bc6ceddd522bbcca8ec0
Isotarge <isaacjmiell@gmail.com>
2016-04-24 22:35:50 +0930

    TS2: Support remaining N64 versions

--- games/ts2.lua
+++ games/ts2.lua
@@ -1,10 +1,11 @@
 local Game = {};
 
-local x_pos;
-local y_pos;
-local z_pos;
-
-local facing_angle;
+Game.Memory = { -- Version order: France, Europe, USA, German 1.1, German 1.0
+	["x_position"] = {0x0BBB40, 0x0BB460, 0x0BB070, 0x0BBB60, 0x0BBB40}, -- s32_be
+	["y_position"] = {0x0BBB44, 0x0BB464, 0x0BB074, 0x0BBB64, 0x0BBB44}, -- s32_be
+	["z_position"] = {0x0BBB48, 0x0BB468, 0x0BB078, 0x0BBB68, 0x0BBB48}, -- s32_be
+	["facing_angle"] = {0x0BBB88, 0x0BB4A8, 0x0BB0B8, 0x0BBBA8, 0x0BBB88}, -- u16_be
+};
 
 --------------------
 -- Region/Version --
@@ -13,20 +14,20 @@ local facing_angle;
 function Game.detectVersion(romName, romHash)
 	if emu.getsystemid() == "N64" then
 		if romHash == "A9F97E22391313095D2C2FBAF81FB33BFA2BA7C6" then -- France
-			-- TODO
+			version = 1;
+			return true;
 		elseif romHash == "92015E5254CBBAD1BC668ECB13A4B568E5F55052" then -- Europe
-			-- TODO
+			version = 2;
+			return true;
 		elseif romHash == "982AD2E1E44C6662C88A77367BC5DF91C51531BF" then -- USA
-			x_pos = 0x0BB070;
-			y_pos = 0x0BB074;
-			z_pos = 0x0BB078;
-
-			facing_angle = 0x0BB0B8;
+			version = 3;
 			return true;
 		elseif romHash == "EAE83C07E2E777D8E71A5BE6120AED03D7E67782" then -- German 1.1
-			-- TODO
+			version = 4;
+			return true;
 		elseif romHash == "F8FBB100227015BE8629243F53D70F29A2A14315" then -- German 1.0
-			-- TODO
+			version = 5;
+			return true;
 		end
 	end
 	return false;
@@ -44,32 +45,36 @@ Game.speedy_invert_Y = true;
 Game.rot_speed = 10;
 Game.max_rot_units = 4096;
 
+function Game.isPhysicsFrame()
+	return not emu.islagged(); -- TODO: Research lag in this game
+end
+
 --------------
 -- Position --
 --------------
 
 function Game.getXPosition()
-	return mainmemory.read_s32_be(x_pos);
+	return mainmemory.read_s32_be(Game.Memory.x_position[version]);
 end
 
 function Game.getYPosition()
-	return mainmemory.read_s32_be(y_pos);
+	return mainmemory.read_s32_be(Game.Memory.y_position[version]);
 end
 
 function Game.getZPosition()
-	return mainmemory.read_s32_be(z_pos);
+	return mainmemory.read_s32_be(Game.Memory.z_position[version]);
 end
 
 function Game.setXPosition(value)
-	mainmemory.write_s32_be(x_pos, value);
+	mainmemory.write_s32_be(Game.Memory.x_position[version], value);
 end
 
 function Game.setYPosition(value)
-	mainmemory.write_s32_be(y_pos, value);
+	mainmemory.write_s32_be(Game.Memory.y_position[version], value);
 end
 
 function Game.setZPosition(value)
-	mainmemory.write_s32_be(z_pos, value);
+	mainmemory.write_s32_be(Game.Memory.z_position[version], value);
 end
 
 --------------
@@ -77,27 +82,27 @@ end
 --------------
 
 function Game.getXRotation()
-	return mainmemory.read_u16_be(facing_angle);
+	return 0; -- TODO
 end
 
 function Game.getYRotation()
-	return mainmemory.read_u16_be(facing_angle);
+	return mainmemory.read_u16_be(Game.Memory.facing_angle[version]);
 end
 
 function Game.getZRotation()
-	return mainmemory.read_u16_be(facing_angle);
+	return 0; -- TODO
 end
 
 function Game.setXRotation(value)
-	mainmemory.write_u16_be(facing_angle, value); -- TODO
+	-- TODO
 end
 
 function Game.setYRotation(value)
-	mainmemory.write_u16_be(facing_angle, value);
+	mainmemory.write_u16_be(Game.Memory.facing_angle[version], value);
 end
 
 function Game.setZRotation(value)
-	mainmemory.write_u16_be(facing_angle, value); -- TODO
+	-- TODO
 end
 
 Game.OSD = {
