0071eee871dd826450d279955890a22600f83c6a
Isotarge <isaacjmiell@gmail.com>
2016-06-06 22:40:35 +0930

    Miracle World: Hitbox display
    Oh boy

--- Beta/Miracle World.lua
+++ Beta/Miracle World.lua
@@ -1,82 +1,95 @@
+mode = "Hitbox"; -- Standard
+show_inactive = true;
+
 local object_array_base = 0x300;
 local object_size = 0x20;
 local object_array_capacity = 30;
 
+local red = 0xFFFF0000;
+local yellow = 0xFFFFFF00;
+local green = 0xFF00FF00;
+local pink = 0xFFFF00FF;
+
 local object_fields = {
 	["object_type"] = 0x00, -- Byte
 	["object_types"] = {
-		[0x01] = "Player",
-		[0x02] = "Bullet",
-		[0x03] = "Explosion", -- Vehicle dying
-		[0x04] = "Bullet", -- Dying
-		[0x0B] = "Janken Choice Display", -- Player
-		[0x0C] = "Janken Score Display",
-		[0x0D] = "Stone Head's Head",
-		[0x0E] = "Scissors Head's Head",
-		[0x0F] = "Paper Head's Head",
-		[0x10] = "Spike", -- Will fall when room loads
-		[0x12] = "Falling Block", -- Loading
-		[0x13] = "Spike", -- Loading
-		[0x14] = "Spike", -- Falling
-		[0x15] = "Waterfall", -- Falling
-		[0x16] = "Trapdoor", -- Opens when stepped on
-		[0x17] = "Trigger", -- For falling blocks
-		--[0x18] = "Unknown 0x18", -- Title Screen
-		[0x19] = "Projectile", -- Janken Ninja Star
-		[0x1B] = "Projectile", -- Ring
-		[0x1A] = "Projectile", -- Scissors Head Ninja Star
-		[0x1C] = "Janken",
-		[0x1D] = "Stone Head",
-		[0x1E] = "Scissors Head",
-		[0x1F] = "Paper Head",
-		[0x20] = "Bat", -- Left
-		[0x22] = "Bubble", -- Big Frog
-		[0x23] = "Big Frog",
-		[0x24] = "Octopus", -- Arm segment
-		[0x2B] = "Dying", -- Small enemy
-		[0x2C] = "Plant Enemy", -- Moves up and down
-		[0x2D] = "Bird", -- Left
-		[0x2E] = "Fish", -- Big Left
-		[0x2F] = "Frog", -- Small, Grounded
-		[0x30] = "Fish", -- Small Left
-		[0x31] = "Seahorse", -- Left
-		[0x32] = "Seahorse", -- Right
-		[0x34] = "Fish", -- Small Right
-		[0x35] = "Fish", -- Big Right
-		[0x36] = "Bat", -- Right
-		[0x33] = "Bird", -- Right
-		[0x37] = "Frog", -- Small, Jumping
-		[0x38] = "Box Particle",
-		[0x39] = "Box Particle",
-		[0x3A] = "Box Particle",
-		[0x3B] = "Box Particle",
-		[0x3C] = "Money",
-		[0x3D] = "Flame",
-		[0x3E] = "Scorpion", -- Left
-		[0x3F] = "Scorpion", -- Right
-		[0x40] = "Cloud",
-		[0x41] = "Cloud", -- Shooting Lightning
-		[0x42] = "Fish", -- Jumping Piranha
-		[0x43] = "Dying", -- Boss, turns into Rice Cake
-		[0x44] = "Rice Cake",
-		[0x45] = "Saint Nurari", -- Level 4
-		[0x46] = "OX", -- Left
-		[0x47] = "OX", -- Left, Hurt
-		[0x48] = "OX", -- Right
-		[0x49] = "OX", -- Right, Hurt
-		[0x4B] = "Hidden Block",
-		[0x4D] = "Extra Life",
-		[0x4E] = "Ring",
-		[0x4F] = "Ghost",
-		[0x50] = "Saint Nurari", -- Level 6
-		[0x51] = "Patricia", -- Level 16
-		[0x52] = "Item", -- Helecopter, Crown, Blue circle with star
-		[0x54] = "Rolling Rock",
-		[0x55] = "Hopper",
-		[0x57] = "Flame", -- Stationary
-		[0x61] = "Crown Code Controller",
+		[0x01] = {["name"] = "Player", ["color"] = yellow},
+		[0x02] = {["name"] = "Bullet", ["hitbox_x"] = 8, ["hitbox_y"] = 8},
+		[0x03] = {["name"] = "Explosion", ["color"] = yellow}, -- Vehicle dying
+		[0x04] = {["name"] = "Bullet"}, -- Dying
+		[0x0B] = {["name"] = "Janken Choice Display", ["hitbox_x"] = 32, ["hitbox_y"] = 32, ["color"] = pink}, -- Player
+		[0x0C] = {["name"] = "Janken Score Display"},
+		[0x0D] = {["name"] = "Stone Head's Head", ["color"] = red},
+		[0x0E] = {["name"] = "Scissors Head's Head", ["color"] = red},
+		[0x0F] = {["name"] = "Paper Head's Head", ["color"] = red},
+		[0x10] = {["name"] = "Spike", ["color"] = red}, -- Will fall when room loads
+		[0x12] = {["name"] = "Falling Block", ["color"] = red}, -- Loading
+		[0x13] = {["name"] = "Spike", ["color"] = red}, -- Loading
+		[0x14] = {["name"] = "Spike", ["color"] = red}, -- Falling
+		[0x15] = {["name"] = "Waterfall", ["color"] = red}, -- Falling
+		[0x16] = {["name"] = "Trapdoor", ["color"] = pink}, -- Opens when stepped on
+		[0x17] = {["name"] = "Trigger", ["color"] = pink}, -- For falling blocks
+		--[0x18] = {["name"] = "Unknown 0x18"}, -- Title Screen
+		[0x19] = {["name"] = "Projectile", ["color"] = red}, -- Janken Ninja Star
+		[0x1B] = {["name"] = "Projectile", ["hitbox_x"] = 8, ["hitbox_y"] = 8}, -- Ring
+		[0x1A] = {["name"] = "Projectile", ["color"] = red}, -- Scissors Head Ninja Star
+		[0x1C] = {["name"] = "Janken", ["color"] = pink},
+		[0x1D] = {["name"] = "Stone Head", ["color"] = pink},
+		[0x1E] = {["name"] = "Scissors Head", ["color"] = pink},
+		[0x1F] = {["name"] = "Paper Head", ["color"] = pink},
+		[0x20] = {["name"] = "Bat", ["direction"] = "Left", ["color"] = red}, -- Left
+		[0x22] = {["name"] = "Bubble", ["color"] = red, ["hitbox_x"] = 8, ["hitbox_y"] = 8}, -- Big Frog
+		[0x23] = {["name"] = "Big Frog", ["color"] = red},
+		[0x24] = {["name"] = "Octopus", ["color"] = red, ["hitbox_x"] = 8, ["hitbox_y"] = 8}, -- Arm segment
+		[0x27] = {["name"] = "Blue Bear", ["color"] = pink},
+		[0x29] = {["name"] = "Projectile", ["color"] = red}, -- Monkey's projectile
+		[0x2A] = {["name"] = "Monkey", ["color"] = red},
+		[0x2B] = {["name"] = "Dying"}, -- Small enemy
+		[0x2C] = {["name"] = "Plant", ["color"] = red}, -- Moves up and down
+		[0x2D] = {["name"] = "Bird", ["direction"] = "Left", ["color"] = red, ["hitbox_x"] = 24, ["hitbox_y"] = 16},
+		[0x2E] = {["name"] = "Fish", ["directoin"] = "Left", ["color"] = red}, -- Big
+		[0x2F] = {["name"] = "Frog", ["color"] = red}, -- Small, Grounded
+		[0x30] = {["name"] = "Fish", ["direction"] = "Left", ["color"] = red}, -- Small Left
+		[0x31] = {["name"] = "Seahorse", ["direction"] = "Left", ["color"] = red},
+		[0x32] = {["name"] = "Seahorse", ["direction"] = "Right", ["color"] = red},
+		[0x34] = {["name"] = "Fish", ["direction"] = "Right", ["color"] = red}, -- Small
+		[0x35] = {["name"] = "Fish", ["direction"] = "Right", ["color"] = red}, -- Big
+		[0x36] = {["name"] = "Bat", ["direction"] = "Right", ["color"] = red},
+		[0x33] = {["name"] = "Bird", ["direction"] = "Right", ["color"] = red, ["hitbox_x"] = 24, ["hitbox_y"] = 16},
+		[0x37] = {["name"] = "Frog", ["color"] = red}, -- Small, Jumping
+		[0x38] = {["name"] = "Box Particle", ["hitbox_x"] = 8, ["hitbox_y"] = 8},
+		[0x39] = {["name"] = "Box Particle", ["hitbox_x"] = 8, ["hitbox_y"] = 8},
+		[0x3A] = {["name"] = "Box Particle", ["hitbox_x"] = 8, ["hitbox_y"] = 8},
+		[0x3B] = {["name"] = "Box Particle", ["hitbox_x"] = 8, ["hitbox_y"] = 8},
+		[0x3C] = {["name"] = "Money", ["color"] = green},
+		[0x3D] = {["name"] = "Flame", ["color"] = red},
+		[0x3E] = {["name"] = "Scorpion", ["direction"] = "Left", ["color"] = red}, -- Also used for flames in later levels
+		[0x3F] = {["name"] = "Scorpion", ["direction"] = "Right", ["color"] = red}, -- Also used for flames in later levels
+		[0x40] = {["name"] = "Cloud", ["color"] = red},
+		[0x41] = {["name"] = "Cloud", ["color"] = red}, -- Shooting Lightning
+		[0x42] = {["name"] = "Flying Fish", ["color"] = red},
+		[0x43] = {["name"] = "Dying", ["color"] = pink}, -- Boss, turns into Rice Cake
+		[0x44] = {["name"] = "Rice Cake", ["color"] = green},
+		[0x45] = {["name"] = "Saint Nurari", ["color"] = yellow}, -- Level 4
+		[0x46] = {["name"] = "OX", ["direction"] = "Left", ["color"] = pink},
+		[0x47] = {["name"] = "OX", ["direction"] = "Left", ["color"] = pink}, -- Hurt
+		[0x48] = {["name"] = "OX", ["direction"] = "Right", ["color"] = pink},
+		[0x49] = {["name"] = "OX", ["direction"] = "Right", ["color"] = pink}, -- Hurt
+		[0x4B] = {["name"] = "Hidden Block", ["color"] = pink},
+		[0x4D] = {["name"] = "Extra Life", ["color"] = pink},
+		[0x4E] = {["name"] = "Ring", ["color"] = pink},
+		[0x4F] = {["name"] = "Ghost", ["color"] = red},
+		[0x50] = {["name"] = "Saint Nurari", ["color"] = yellow}, -- Level 6
+		[0x51] = {["name"] = "Patricia", ["color"] = yelow}, -- Level 16
+		[0x52] = {["name"] = "Item", ["color"] = pink}, -- Helecopter, Crown, Blue circle with star
+		[0x54] = {["name"] = "Rolling Rock", ["color"] = red},
+		[0x55] = {["name"] = "Hopper", ["color"] = red},
+		[0x56] = {["name"] = "Arrow", ["hitbox_x"] = 8, ["hitbox_y"] = 8}, -- Map
+		[0x57] = {["name"] = "Flame", ["color"] = red}, -- Stationary
+		[0x61] = {["name"] = "Crown Code Controller", ["color"] = pink},
 	},
 	["state"] = 0x01, -- Byte
+	["active"] = 0x09, -- u16, 0x0000
 	["x_position"] = 0x0C, -- Byte
 	["y_position"] = 0x0E, -- Byte
 	["x_velocity"] = 0x10, -- S8
@@ -104,33 +117,76 @@ function draw_ui()
 	local gui_y = 2;
 	local row = 0;
 	local height = 16;
+	local mouse = input.getmouse();
+	if mode == "Hitbox" then
+		gui.clearGraphics();
+	end
+
+	-- Draw mouse pixel
+	--gui.drawPixel(mouse.X, mouse.Y, red);
 
 	for i = 0, object_array_capacity do
 		local objectBase = object_array_base + (i * object_size);
 		local objectType = mainmemory.readbyte(objectBase + object_fields.object_type);
+		local objectActive = mainmemory.read_u16_be(objectBase + object_fields.active);
+		local objectTypeTable = nil;
 		local color = nil;
 		if objectType ~= 0 then
-			if objectType == 0x52 then
-				if mainmemory.readbyte(objectBase + 0x07) == 0xD3 and mainmemory.readbyte(objectBase + 0x08) == 0x80 then -- Detect crown and make it flash Red & Yellow
-					if emu.framecount() % 10 > 4 then
-						color = 0xFFFF0000; -- Red
-					else
-						color = 0xFFFFFF00; -- Yellow
-					end
+			-- Default to 16 width/height for hitbox
+			local hitboxX = 16;
+			local hitboxY = 16;
+
+			-- Get the X and Y position of the object
+			local xPosition = mainmemory.readbyte(objectBase + object_fields.x_position);
+			local yPosition = mainmemory.readbyte(objectBase + object_fields.y_position);
+
+			if type(object_fields.object_types[objectType]) == "table" then
+				objectTypeTable = object_fields.object_types[objectType];
+
+				if type(objectTypeTable.name) == "string" then
+					objectType = object_fields.object_types[objectType].name.." "..toHexString(objectType);
+				else
+					objectType = "Unknown ("..toHexString(objectType)..")";
 				end
-			end
-			if objectType == 0x44 then -- Detect Rice Cake and color it green
-				color = 0xFF00FF00; -- Green
-			end
-			if type(object_fields.object_types[objectType]) ~= "nil" then
-				objectType = object_fields.object_types[objectType];
+
+				if type(objectTypeTable["color"]) == "number" then
+					color = objectTypeTable["color"];
+				end
+
+				if type(objectTypeTable.hitbox_x) == "number" then
+					hitboxX = objectTypeTable.hitbox_x;
+				end
+				if type(objectTypeTable.hitbox_y) == "number" then
+					hitboxY = objectTypeTable.hitbox_y;
+				end
 			else
 				objectType = "Unknown ("..toHexString(objectType)..")";
 			end
-			local xPosition = mainmemory.readbyte(objectBase + object_fields.x_position);
-			local yPosition = mainmemory.readbyte(objectBase + object_fields.y_position);
-			gui.text(gui_x, gui_y + height * row, xPosition..", "..yPosition.." - "..objectType.." "..toHexString(objectBase), color, nil, 'bottomright');
-			row = row + 1;
+
+			if objectType == 0x52 then
+				if mainmemory.readbyte(objectBase + 0x07) == 0xD3 and mainmemory.readbyte(objectBase + 0x08) == 0x80 then -- Detect crown and make it flash Red & Yellow
+					if emu.framecount() % 10 > 4 then
+						color = red;
+					else
+						color = yellow;
+					end
+				end
+			end
+
+			if mode == "Standard" then
+				gui.text(gui_x, gui_y + height * row, xPosition..", "..yPosition.." - "..objectType.." "..toHexString(objectBase), color, nil, 'bottomright');
+				row = row + 1;
+			end
+
+			if mode == "Hitbox" then
+				if showInactive or objectActive == 0x00 then
+					if (mouse.X >= xPosition and mouse.X <= xPosition + hitboxX) and (mouse.Y >= yPosition and mouse.Y <= yPosition + hitboxY) then
+						gui.drawText(xPosition, yPosition, objectType, color);
+						gui.drawText(xPosition, yPosition + height, toHexString(objectBase).." "..xPosition..","..yPosition, color);
+					end
+					gui.drawRectangle(xPosition, yPosition, hitboxX, hitboxY, color); -- Draw the object's hitbox
+				end
+			end
 		end
 	end
 end
