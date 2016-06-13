75b1c40bfec1c8e0c1f7a32c327c84e652aa15a8
Isotarge <isaacjmiell@gmail.com>
2016-06-14 04:19:47 +0930

    The Ninja: More object documentation

--- Beta/The Ninja.lua
+++ Beta/The Ninja.lua
@@ -24,40 +24,67 @@ local object_fields = {
 		[0x01] = {["name"] = "Player", ["color"] = yellow},
 		[0x02] = {["name"] = "Player Projectile", ["color"] = yellow},
 		[0x03] = {["name"] = "Player Projectile", ["color"] = yellow},
-		[0x04] = {["name"] = "Boss", ["color"] = pink},
+		[0x04] = {["name"] = "Boss", ["color"] = pink}, -- Early levels
+		[0x05] = {["name"] = "Boss", ["color"] = pink}, -- Later levels
 		[0x06] = {["name"] = "Boss Projectile", ["isEnemyProjectile"] = true, ["color"] = red},
+		[0x07] = {["name"] = "Boss Projectile", ["isEnemyProjectile"] = true, ["color"] = red}, -- Level 5
 		[0x08] = {["name"] = "Boss Projectile", ["isEnemyProjectile"] = true, ["color"] = red},
 		[0x09] = {["name"] = "Enemy Projectile", ["isEnemyProjectile"] = true, ["color"] = red}, -- Small projectile
+		[0x0A] = {["name"] = "Boss Projectile", ["isEnemyProjectile"] = true, ["color"] = red},
 		[0x0B] = {["name"] = "Red Scroll", ["color"] = pink},
 		[0x0C] = {["name"] = "Blue Scroll", ["color"] = pink},
 		[0x0D] = {["name"] = "Green Scroll", ["color"] = pink},
+		[0x0E] = {["name"] = "Splash"},
 		[0x0F] = {["name"] = "Enemy Dying", ["isEnemy"] = true},
 		[0x10] = {["name"] = "Grey Enemy", ["isEnemy"] = true},
 		[0x11] = {["name"] = "Blue Enemy", ["isEnemy"] = true},
 		[0x12] = {["name"] = "Grey Enemy", ["isEnemy"] = true},
 		[0x13] = {["name"] = "Grey Enemy", ["isEnemy"] = true},
+		[0x14] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- Level 6
+		[0x15] = {["name"] = "Grey Enemy", ["isEnemy"] = true},
 		[0x16] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- Scythe
 		[0x17] = {["name"] = "Boulder Enemy", ["isEnemy"] = true},
 		[0x18] = {["name"] = "Popup Enemy", ["isEnemy"] = true}, -- Level 2
-		[0x19] = {["name"] = "Bouncing Boulder", ["isEnemy"] = true}, -- Level 2
+		[0x19] = {["name"] = "Bouncing Boulder", ["color"] = red}, -- Level 2
 		[0x1A] = {["name"] = "Bouncing Boulder Shadow"}, -- Level 2
+		[0x1B] = {["name"] = "Horse", ["color"] = red}, -- Level 7
 		[0x1C] = {["name"] = "Wolf", ["isEnemy"] = true},
+		[0x1D] = {["name"] = "Light Blue Enemy", ["isEnemy"] = true}, -- Level 11
+		[0x1E] = {["name"] = "Green Enemy", ["isEnemy"] = true}, -- Level 8
 		[0x1F] = {["name"] = "Enemy Projectile", ["isEnemyProjectile"] = true, ["color"] = red}, -- Scythe
-		[0x20] = {["name"] = "Grey Enemy"}, -- From boulder
+		[0x20] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- From boulder
+
+		[0x22] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- Circling
+		[0x23] = {["name"] = "Fire Enemy", ["isEnemy"] = true}, -- Level 7
 		[0x24] = {["name"] = "Red Jumping Enemy", ["isEnemy"] = true}, -- Level 4
-		[0x25] = {["name"] = "Red Running Enemy", ["isEnemy"] = true}, -- Level 4
-		[0x26] = {["name"] = "Red Running Enemy", ["isEnemy"] = true}, -- Level 4, after jumping
-		[0x2C] = {["name"] = "Statue", ["color"] = pink}, -- Level 4
+		[0x25] = {["name"] = "Red Enemy", ["isEnemy"] = true}, -- Level 4
+		[0x26] = {["name"] = "Red Enemy", ["isEnemy"] = true}, -- Level 4, after jumping
+		[0x27] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- Cliff
+		[0x28] = {["name"] = "Blue Enemy", ["isEnemy"] = true}, -- Cliff
+		[0x29] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- Cliff, moving up
+		[0x2A] = {["name"] = "Ball Spawner"}, -- Cliff
+		[0x2B] = {["name"] = "Ball", ["isEnemyProjectile"] = true, ["color"] = red}, -- Cliff
+		[0x2C] = {["name"] = "Green Scroll Trigger", ["color"] = pink},
 		[0x2D] = {["name"] = "Boulder Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Contains green scroll
 		[0x2E] = {["name"] = "Arrow", ["color"] = pink}, -- Map Screen
 		[0x2F] = {["name"] = "Red Scroll", ["color"] = pink}, -- Map Screen
 		[0x30] = {["name"] = "Blue Scroll", ["color"] = pink}, -- Map Screen
-		[0x31] = {["name"] = "Green Scroll", ["color"] = pink}, -- Map Screen
+		[0x31] = {["name"] = "Green Scrolls", ["color"] = pink}, -- Map Screen
+		[0x32] = {["name"] = "Staircase Trigger", ["color"] = pink}, -- Level 10
+
 		[0x35] = {["name"] = "Boulder Enemy", ["isEnemy"] = true},
 		[0x36] = {["name"] = "Blue Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Contains red scroll
 		[0x37] = {["name"] = "Blue Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Contains blue scroll
 		[0x38] = {["name"] = "Wolf", ["isEnemy"] = true, ["color"] = pink}, -- Contains red scroll
 		[0x39] = {["name"] = "Grey Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Contains blue scroll
+		[0x3A] = {["name"] = "Red Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Level 6, Contains red scroll
+
+		[0x3E] = {["name"] = "Light Blue Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Level 11, Contains red scroll
+		[0x3F] = {["name"] = "Light Blue Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Level 11, Contains blue scroll
+		[0x40] = {["name"] = "Grey Enemy", ["isEnemy"] = true}, -- Level 5
+		[0x41] = {["name"] = "Grey Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Level 5, Contains red scroll
+		[0x42] = {["name"] = "Fire Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Level 7, Contains red scroll
+		[0x43] = {["name"] = "Grey Enemy", ["isEnemy"] = true, ["color"] = pink}, -- Cliff, Contains red scroll
 	},
 	["animation_index"] = 0x03, -- Byte
 	["animation_current_frame"] = 0x04, -- Byte
