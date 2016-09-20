44d7c4874d05dc1284e812c9cccddc04ac67ee6b
Isotarge <isaacjmiell@gmail.com>
2016-09-20 12:09:19 +0930

    TFTT: Split mouse control in to a separate script

--- Beta/TFTT Log.lua
+++ Beta/TFTT Log.lua
@@ -54,18 +54,6 @@ function draw_OSD()
 			row = row + 1;
 		end
 	end
-
-	-- Make game cursor follow real cursor
-	local mousePos = input.getmouse();
-	mainmemory.write_u16_be(cursorX, mousePos.X * 2);
-	mainmemory.write_u16_be(cursorY, (mousePos.Y * 2) - 40); -- Minus 40 pixels to compensate for Overscan
-
-	if mousePos.Left then
-		joypad.set({["B"] = true}, 1);
-	end
-	if mousePos.Right then
-		joypad.set({["C"] = true}, 1);
-	end
 end
 
 event.onframestart(draw_OSD);
