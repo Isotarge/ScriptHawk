f4ad5e7fd010684b99bc0b1978ffbdd8285e8efe
Isotarge <isaacjmiell@gmail.com>
2016-09-22 23:00:03 +0930

    TFTT: Lots more sector data
    - Figured out the standard format for tickers and found a bunch of them
    - Function to recursively print a table, for quick debugging of dumped
    sector data
    - Fill in some blanks in the researchTypes table

--- Beta/TFTT Log.lua
+++ Beta/TFTT Log.lua
@@ -8,6 +8,24 @@ function toHexString(value, desiredLength, prefix)
 	return prefix..value;
 end
 
+--[[ rPrint(struct, [limit], [indent])   Recursively print arbitrary data. 
+Set limit (default 100) to stanch infinite loops.
+Indents tables as [KEY] VALUE, nested tables as [KEY] [KEY]...[KEY] VALUE
+Set indent ("") to prefix each line:    Mytable [KEY] [KEY]...[KEY] VALUE
+--]]
+function rPrint(s, l, i) -- recursive Print (structure, limit, indent)
+	l = (l) or 100; i = i or "";	-- default item limit, indent string
+	if (l<1) then print "ERROR: Item limit reached."; return l-1 end;
+	local ts = type(s);
+	if (ts ~= "table") then print (i,ts,s); return l-1 end
+	print (i,ts);           -- print "table"
+	for k,v in pairs(s) do  -- print "[KEY] VALUE"
+		l = rPrint(v, l, i.."\t["..tostring(k).."]");
+		if (l < 0) then break end
+	end
+	return l
+end	
+
 local selectedSectorPointer = 0xB5B4;
 local sectorBase = 0xB6C4;
 local sectorSize = 0x44A;
@@ -33,8 +51,8 @@ local characterColors = {
 local epochs = {
 	[0] = "9500BC",
 	[1] = "3000BC",
-	[2] = "100BC",
-	[3] = "900AD",
+	[2] = " 100BC", -- TODO: Sort out padding w/table renderer library
+	[3] = " 900AD",
 	[4] = "1400AD",
 	[5] = "1850AD",
 	[6] = "1915AD",
@@ -61,11 +79,13 @@ local researchTypes = {
 		[0] = "1 Shield",
 		[1] = "2 Shield",
 		[2] = "3 Shield",
-		--
-		[4] = "3H Shield",
-		[5] = "4H Shield", -- TODO: This can be 1H and 2H sheld too?
-		--
-		[7] = "4H Shield",
+		[3] = "4 Shield",
+		[4] = "5 Shield",
+		[5] = "6 Shield",
+		[6] = "7 Shield",
+		[7] = "8 Shield",
+		[8] = "9 Shield",
+		[9] = "10 Shield",
 	},
 	[1] = { -- Defense
 		[0] = "Stick",
@@ -76,6 +96,8 @@ local researchTypes = {
 		[5] = "Musket",
 		[6] = "Machine Gun",
 		[7] = "Bazooka",
+		[8] = "Nuke Defence",
+		[9] = "Laser",
 	},
 	[2] = { -- Weapon
 		[0] = "Rock",
@@ -87,6 +109,7 @@ local researchTypes = {
 		[6] = "Plane",
 		[7] = "Jet",
 		[8] = "Nuke",
+		[9] = "UFO", -- Spaceship
 	},
 };
 
@@ -104,13 +127,26 @@ function getResearchString(data)
 end
 
 local sectorData = {
-	["breed_ticker"] = 0xBA, -- 12.4 fixed point (u16_be / 16)
-	["breed_population"] = 0xBC, -- u16_be
+	["ticker"] = {
+		["pop"] = 0x00, -- u16_be
+		["ticker"] = 0x08, -- 12.4 fixed point (u16_be / 16)
+		["pop_scaled"] = 0x0A, -- u16_be
+	},
+	["tickers"] = {
+		["tower_construction"] = {
+			["scarlet"] = 0x10,
+			["caesar"] = 0x28,
+			["oberon"] = 0x40,
+			["madcap"] = 0x58,
+		},
+		["mine_construction"] = 0x70,
+		["lab_construction"] = 0x88,
+		["factory_construction"] = 0xA0,
+		["breed"] = 0xB2,
+		["research"] = 0xC4,
+	},
 	["research_type"] = 0xBE, -- u16_be
 	["research_index"] = 0xC0, -- u16_be
-	["research_pop"] = 0xC4, -- u16_be
-	["research_ticker"] = 0xCC, -- u16_be
-	["research_pop_2"] = 0xCE, -- u16_be
 	["army_bases"] = {
 		["scarlet"] = 0x2C8,
 		["caesar"] = 0x2D3,
@@ -166,10 +202,31 @@ function getArmyData(army)
 	};
 end
 
+function getTickerData(ticker)
+	return {
+		["pop"] = mainmemory.read_u16_be(ticker + sectorData.ticker.pop),
+		["ticker"] = mainmemory.read_u16_be(ticker + sectorData.ticker.ticker) / 16,
+		["pop_scaled"] = mainmemory.read_u16_be(ticker + sectorData.ticker.pop_scaled),
+	};
+end
+
 function getSectorData(sector)
 	local data = {};
-	data.breed_ticker = mainmemory.read_u16_be(sector + sectorData.breed_ticker) / 16;
-	data.breed_population = mainmemory.read_u16_be(sector + sectorData.breed_population);
+
+	data.tickers = {
+		["tower_construction"] = {
+			["scarlet"] = getTickerData(sector + sectorData.tickers.tower_construction.scarlet),
+			["caesar"] = getTickerData(sector + sectorData.tickers.tower_construction.caesar),
+			["oberon"] = getTickerData(sector + sectorData.tickers.tower_construction.oberon),
+			["madcap"] = getTickerData(sector + sectorData.tickers.tower_construction.madcap),
+		},
+		["mine_construction"] = getTickerData(sector + sectorData.tickers.mine_construction),
+		["lab_construction"] = getTickerData(sector + sectorData.tickers.lab_construction),
+		["factory_construction"] = getTickerData(sector + sectorData.tickers.factory_construction),
+		["breed"] = getTickerData(sector + sectorData.tickers.breed),
+		["research"] = getTickerData(sector + sectorData.tickers.research),
+	};
+
 	data.population = mainmemory.read_u16_be(sector + sectorData.population);
 	data.epoch = mainmemory.read_u16_be(sector + sectorData.epoch);
 	data.owner = mainmemory.read_u16_be(sector + sectorData.owner);
@@ -198,6 +255,10 @@ function getSectorData(sector)
 	return data;
 end
 
+function printSectorData(sector)
+	rPrint(getSectorData(sector));
+end
+
 function getArmyString(data)
 	local armyString = "armies: ";
 	armyString = armyString..data.army.scarlet.total..",";
@@ -220,6 +281,7 @@ function draw_OSD()
 			rowString = rowString.."pop: "..data.population.." ";
 			rowString = rowString..getResearchString(data).." ";
 			rowString = rowString..data.tower_health.."/"..data.max_tower_health.."HP ";
+			--rowString = rowString.."owner: "..data.owner.." ";
 			rowString = rowString..epochs[data.epoch];
 
 			gui.text(OSDPosition[1] + 7 * OSDCharacterWidth, OSDPosition[2] + row * OSDRowHeight, rowString, nil, "bottomright");
