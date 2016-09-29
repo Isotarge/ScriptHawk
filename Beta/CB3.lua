1c90430fe54b93722b18c0fce20f90fc7a408c82
Isotarge <isaacjmiell@gmail.com>
2016-09-29 20:11:44 +0930

    CB3: Fix some labels

--- Beta/CB3.lua
+++ Beta/CB3.lua
@@ -8,54 +8,57 @@ function drawOSD()
 	local yOffset = 70;
 	local height = 16;
 
-	local addr = mainmemory.read_u32_le(HEADER);
-	if addr >= 0x80000000 and addr <= 0x80200000 then
-		addr = addr - 0x80000000;
+	local actor = mainmemory.read_u32_le(HEADER);
+	if actor >= 0x80000000 and actor <= 0x80200000 then
+		actor = actor - 0x80000000;
 	else
 		gui.text(xOffset, yOffset + row * height, "Crash not found...");
 		return;
 	end
 
-	local X  = mainmemory.read_s32_le(addr + 0x60) / pScale;
-	local Y  = mainmemory.read_s32_le(addr + 0x68) / pScale;
-	local Z  = mainmemory.read_s32_le(addr + 0x64) / pScale;
-	local XV = mainmemory.read_s32_le(addr + 0x84) / vScale;
-	local YV = mainmemory.read_s32_le(addr + 0x8C) / vScale;
-	local ZV = mainmemory.read_s32_le(addr + 0x88) / vScale;
-	local V  = mainmemory.read_s32_le(addr + 0x104) / vScale;
-	local XY = math.sqrt(XV*XV+YV*YV);
-	local D  = mainmemory.read_s32_le(addr + 0x94) / 4096*360;
-	local J  = mainmemory.read_u32_le(addr + 0x1B5);
+	local X  = mainmemory.read_s32_le(actor + 0x60) / pScale;
+	local Y  = mainmemory.read_s32_le(actor + 0x64) / pScale;
+	local Z  = mainmemory.read_s32_le(actor + 0x68) / pScale;
+	local XV = mainmemory.read_s32_le(actor + 0x84) / vScale;
+	local YV = mainmemory.read_s32_le(actor + 0x88) / vScale;
+	local ZV = mainmemory.read_s32_le(actor + 0x8C) / vScale;
+	local V  = mainmemory.read_s32_le(actor + 0x104) / vScale;
+	local XZ = math.sqrt(XV*XV + ZV*ZV);
+	local D  = mainmemory.read_s32_le(actor + 0x94) / 4096*360;
+	local J  = mainmemory.read_u32_le(actor + 0x1B5);
 	local BOXi = mainmemory.read_u32_le(0x6CC69);
 	local BOXs = mainmemory.read_u32_le(0x6CDC1);
 	local Level = mainmemory.read_u32_le(0x618DC);
 
-	gui.text(xOffset, yOffset + row * height, string.format("%8x : Header", addr));
+	gui.text(xOffset, yOffset + row * height, string.format("%8X : Crash Pointer", actor));
 	row = row + 1;
 	gui.text(xOffset, yOffset + row * height, string.format("%8d : Level", Level));
 	row = row + 1;
 	gui.text(xOffset, yOffset + row * height, string.format("%4d/%3d : Box", BOXi, BOXs));
 	row = row + 2;
 
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Z  Pos", Z));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : X Pos", X));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Z  Vel", ZV));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Y Pos", Y));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : X  Pos", X));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Z Pos", Z));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : X  Vel", XV));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Facing", D));
+	row = row + 2;
+
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : X Vel", XV));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Y  Pos", Y));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Y Vel", YV));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Y  Vel", YV));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Z Vel", ZV));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : XY Vel", XY));
+	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : XZ Vel", XZ));
 	row = row + 1;
 	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Velocity", V));
 	row = row + 1;
 	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Jumps", J));
 	row = row + 1;
-	gui.text(xOffset, yOffset + row * height, string.format("%8.2f : Degrees", D));
+	
 end
 
 event.onframestart(drawOSD);
