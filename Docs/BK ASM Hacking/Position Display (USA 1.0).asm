12b338ea4213a13414601dd2233db5a5c3e00305
Isotarge <isaacjmiell@gmail.com>
2016-10-23 00:34:09 +1030

    BK ASM Improvements
    - Port position display to Japanese version
    - Reuse logic for position display between versions
    - Some documentation for PAL and Japanese functions

--- Docs/BK ASM Hacking/Position Display (USA 1.0).asm
+++ Docs/BK ASM Hacking/Position Display (USA 1.0).asm
@@ -1,13 +1,13 @@
 // Hook
-.org 0x8033544C
+.org 0x80334FFC
 JAL 0x80400000
 
-/*HOOK USED WAS 0x8033544C
+/*HOOK USED WAS 0x80334FFC
 |    hook was a JAL to a null_function in the same function that updates the playersPositionVel, etc
 |    NOTE: Since player only updates when game is in normalUpdateMode: 2 (not paused),
 |          the hook will not catch when game it paused
 */
 
 //FUNCTIONS & VARIABLES
-.include "Docs/BK ASM Hacking/BK_PAL.S"
+.include "Docs/BK ASM Hacking/BK_NTSC.S"
 .include "Docs/BK ASM Hacking/Position Display.asm"
--- Docs/BK ASM Hacking/Position Display.asm
+++ Docs/BK ASM Hacking/Position Display.asm
@@ -1,16 +1,3 @@
-// Hook
-.org 0x80334FFC
-JAL 0x80400000
-
-/*HOOK USED WAS 0x80334FFC
-|    hook was a JAL to a null_function in the same function that updates the playersPositionVel, etc
-|    NOTE: Since player only updates when game is in normalUpdateMode: 2 (not paused),
-|          the hook will not catch when game it paused
-*/
-
-//FUNCTIONS & VARIABLES
-.include "Docs/BK ASM Hacking/BK_NTSC.S"
-
 //CONSTANTS
 [OSDXOffset]: 0x08
 
