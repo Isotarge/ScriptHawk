34f7fb444a5becc8c1987e801b5cadc05427dfcb
Isotarge <isaacjmiell@gmail.com>
2016-12-04 19:04:10 +1030

    Impossible Mission: Start populating some tables

--- Beta/See Levels.lua
+++ Beta/See Levels.lua
@@ -22,28 +22,68 @@ local gametime = {
 	centiseconds = 0x00CD,
 };
 
-local function fillMinimap()
-	if mainmemory.readbyte(gametime.minutes) == 0 and mainmemory.readbyte(gametime.seconds) == 0 then
-		return;
-	end
-
-	local value;
-	for i = 0, num_maps do
-		value = mainmemory.readbyte(map_base + i);
-		mainmemory.writebyte(map_base + i, bit.bor(value, 0x80));
-	end
-end
-
-object_types = {
-	-- TODO
-};
-
 maps = {
+	[0x0F] = "Ending",
+	[0x1E] = "Puzzle", -- Light green
+	[0x1F] = "Puzzle", -- Dark Grey
+	[0x20] = "Glitchy Puzzle", -- Light Grey
+	[0x21] = "Glitchy Puzzle", -- Yellow
+	[0x22] = "Glitchy Puzzle", -- Green
+	[0x23] = "Glitchy Puzzle", -- Cyan
 	[0x7F] = "Empty",
 };
 
+object = {
+	obj_type = 0x00,
+	obj_types = {
+		--[0x00] = "??",
+		[0x01] = "Blue Computer",
+		[0x02] = "Printer",
+		[0x03] = "Oven",
+		[0x04] = "Desk",
+		[0x05] = "Bed",
+		[0x06] = "Drawers",
+		[0x07] = "Fireplace ",
+		[0x08] = "Sideways blue monitor",
+		[0x09] = "Pink Chair",
+		[0x0A] = "Flashing brown and blue rectangle",
+		[0x0B] = "Brown and blue box",
+		[0x0C] = "Bookshelf",
+		[0x0D] = "Roll on deodorant",
+		[0x0E] = "Lamp",
+		[0x0F] = "Lounge",
+		[0x10] = "Sink",
+		[0x11] = "Small Pink thing",
+		[0x12] = "Bathtub",
+		[0x13] = "Toilet",
+		[0x14] = "Candy",
+		[0x15] = "End of Game Object",
+		[0x16] = "Microwave",
+		[0x17] = "Desk 2",
+		[0x18] = "Fridge",
+		[0x19] = "*Crash",
+		--
+		[0x1D] = "A2600 mode crash",
+	},
+	x_position = 0x01,
+	y_position = 0x02,
+	contents = 0x03,
+	content_types = {
+		[0x00] = "Nothing",
+		[0x40] = "Lift Init",
+		[0x80] = "Snooze",
+		[0xC0] = "Puzzle Piece",
+	},
+};
+
 object_arrays = { -- Use indexes from maps array
-	
+	[0x01] = {start=0x11EC, finish=0x11F9, objects=3, exits="dr"},
+	[0x02] = {start=0x1201, finish=0x1216, objects=5, exits="dr"},
+	[0x03] = {start=0x121A, finish=0x1223, objects=2, exits="dl,ur"},
+	[0x04] = {start=0x1227, finish=0x123E, objects=6, exits="dl,ur"},
+	[0x05] = {start=0x1244, finish=0x1267, objects=9, exits="ul"},
+	[0x06] = {start=0x126D, finish=0x1280, objects=5, exits="dl,ur"},
+	[0x07] = {start=0x1286, finish=0x1299, objects=5, exits="dr"},
 }; 
 
 function dumpMaps()
@@ -57,5 +97,17 @@ function dumpMaps()
 		end
 	end
 end
+dumpLevels = dumpMaps;
 
+local function fillMinimap()
+	if mainmemory.readbyte(gametime.minutes) == 0 and mainmemory.readbyte(gametime.seconds) == 0 then
+		return;
+	end
+
+	local value;
+	for i = 0, num_maps do
+		value = mainmemory.readbyte(map_base + i);
+		mainmemory.writebyte(map_base + i, bit.bor(value, 0x80));
+	end
+end
 event.onframestart(fillMinimap, "ScriptHawk - Fill minimap");
