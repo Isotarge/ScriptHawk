fc40a1f2331cc1913da4c3066175ac073abaf14e
Isotarge <isaacjmiell@gmail.com>
2017-01-04 03:52:48 +1030

    SFR2049 Improvements
    - Add Velocity to OSD
    - Add a button to unlock cheats
    - Add a few addresses to RAM watches

--- games/rush_2049.lua
+++ games/rush_2049.lua
@@ -4,6 +4,8 @@ local Game = {
 	rot_speed = 100,
 	max_rot_units = 65535,
 	Memory = { -- Version order: N64 USA, N64 PAL
+		["cheat_base"] = {0x11648C, 0x118E84},
+		["number_of_cheats"] = {22, 24},
 		["velocity"] = {0x14A298, 0x170D70},
 		["x_position"] = {0x14A47C, 0x170F54},
 		["y_position"] = {0x14A480, 0x170F58},
@@ -25,6 +27,12 @@ function Game.detectVersion(romName, romHash)
 	end
 
 	return true;
+end
+
+function Game.unlockCheats()
+	for i = 1, Game.Memory.number_of_cheats[version] do
+		mainmemory.writebyte(Game.Memory.cheat_base[version] + i - 1, 0x01);
+	end
 end
 
 function Game.getXPosition()
@@ -58,5 +66,27 @@ end
 function Game.setVelocity(value)
 	return mainmemory.writefloat(Game.Memory.velocity[version], value, true);
 end
+
+function Game.initUI()
+	ScriptHawk.UI.form_controls["Unlock Cheats Button"] = forms.button(ScriptHawk.UI.options_form, "Unlock Cheats", Game.unlockCheats, ScriptHawk.UI.col(10), ScriptHawk.UI.row(0), ScriptHawk.UI.col(4) + 10, ScriptHawk.UI.button_height);
+end
+
+Game.OSD = {
+	{"X", Game.getXPosition},
+	{"Y", Game.getYPosition},
+	{"Z", Game.getZPosition},
+	{"Separator", 1},
+	{"Velocity", Game.getVelocity},
+	{"dY"},
+	{"dXZ"},
+	{"Separator", 1},
+	{"Max dY"},
+	{"Max dXZ"},
+	{"Odometer"},
+	{"Separator", 1},
+	{"Rot. X", Game.getXRotation},
+	{"Facing", Game.getYRotation},
+	{"Rot. Z", Game.getZRotation},
+};
 
 return Game;
