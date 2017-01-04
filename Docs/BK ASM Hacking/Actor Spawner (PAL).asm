76da2f45d4a25342e8eb89b3eea73381608f4d6f
Isotarge <isaacjmiell@gmail.com>
2017-01-04 16:30:00 +1030

    BK: Find spawnAndCarryActor() on all versions

--- Docs/BK ASM Hacking/Actor Spawner (PAL).asm
+++ Docs/BK ASM Hacking/Actor Spawner (PAL).asm
@@ -4,6 +4,6 @@ JAL 0x80400000
 
 [Return]: 0x8024E070
 [SpawnActor]: 0x80328594 ;Inputs: (word $a0 ActorIndex), (float* $a1 LocationXPtr), (float $a2 rotation)
-[SpawnAndCarryActor]: 0x80328594 ;Inputs: (word $a0 ActorIndex), (float* $a1 LocationXPtr) ;TODO: Find this function
+[SpawnAndCarryActor]: 0x8028DC2C ;Inputs: (word $a0 ActorIndex), (float* $a1 LocationXPtr)
 
 .include "Actor Spawner.asm"
