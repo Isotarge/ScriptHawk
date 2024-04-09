fa3ec96ce9a75816f34831b7834753e7119fa11e
Isaac Miell <isaacjmiell@gmail.com>
2024-04-09 21:58:41 +0930

    Update sonic_all_stars_racing.lua
    xz velocity

--- games/sonic_all_stars_racing.lua
+++ games/sonic_all_stars_racing.lua
@@ -21,7 +21,8 @@ local Game = {
 	squish_memory_table = true,
 	Memory = { -- Version order: US, Europe
 		player_pointer = {0x236238, 0},
-		velocity = {0, 0},
+		x_velocity = {0xA7C, 0}, -- s16.16le, relative to player
+		z_velocity = {0xA84, 0}, -- s16.16le, relative to player
 		x_position = {0x6C, 0}, -- s16.16le, relative to player
 		y_position = {0x70, 0}, -- s16.16le, relative to player
 		z_position = {0x74, 0}, -- s16.16le, relative to player
@@ -77,12 +78,51 @@ function Game.setZPosition(value)
 	end
 end
 
+function Game.getXVelocity()
+	local player = Game.getPlayer();
+	if isRAM(player) then
+		return mainmemory.read_s1616_le(player + Game.Memory.x_velocity);
+	end
+	return 0;
+end
+
+function Game.setXVelocity(value)
+	local player = Game.getPlayer();
+	if isRAM(player) then
+		return mainmemory.write_s1616_le(player + Game.Memory.x_velocity, value);
+	end
+end
+
+function Game.getZVelocity()
+	local player = Game.getPlayer();
+	if isRAM(player) then
+		return mainmemory.read_s1616_le(player + Game.Memory.z_velocity);
+	end
+	return 0;
+end
+
+function Game.setZVelocity(value)
+	local player = Game.getPlayer();
+	if isRAM(player) then
+		return mainmemory.write_s1616_le(player + Game.Memory.z_velocity, value);
+	end
+end
+
+function Game.getVelocity()
+	return math.abs(Game.getXVelocity()) + math.abs(Game.getZVelocity());
+end
+
 Game.OSD = {
 	{"Player", hexifyOSD(Game.getPlayer, 6)},
 	{"Separator"},
 	{"X", category="position"},
 	{"Y", category="position"},
 	{"Z", category="position"},
+	{"Separator"},
+	{"X Velocity", Game.getXVelocity, category="position"},
+	{"Z Velocity", Game.getZVelocity, category="position"},
+	{"Separator"},
+	{"Velocity", Game.getVelocity, category="position"},
 	{"Separator"},
 	{"dY", category="positionStats"},
 	{"dXZ", category="positionStats"},
